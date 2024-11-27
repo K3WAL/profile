fb1f9dd961121e9a6edae74ea4d5747ae88a9189
Kewal Uchiha <kewaltamang73@gmail.com>
2024-11-27 21:08:05 +0100

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.svg";
+import kewal from "./logo.png";
+import Designer from "./Designer.png";
+
+export { hero, meta, shopify, starbucks, tesla, logo, kewal, Designer };
